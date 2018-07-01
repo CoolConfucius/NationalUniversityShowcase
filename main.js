8e52784862b952483d929c75265803eb4a2ed61c
Benjamin Huang <benjaminthuang@gmail.com>
2018-06-30 18:26:54 -0700

    added artifact routes from itl 520 522 and more to main js

--- main.js
+++ main.js
@@ -6,8 +6,23 @@ app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {url: '/', templateUrl: './html/home.html' })
     .state('tpes', {url: '/tpes', templateUrl: './html/tpes.html' })
+
+    .state('itl520academictexts', {url: '/itl520academictexts', templateUrl: './html/artifacts/itl520academictexts.html' })
+    .state('itl520contentareathinking', {url: '/itl520contentareathinking', templateUrl: './html/artifacts/itl520contentareathinking.html' })
+    
+    .state('itl522pbl', {url: '/itl522pbl', templateUrl: './html/artifacts/itl522pbl.html' })
+    .state('itl522teaching', {url: '/itl522teaching', templateUrl: './html/artifacts/itl522teaching.html' })
+    
+    .state('itl600comparativeessay', {url: '/itl600comparativeessay', templateUrl: './html/artifacts/itl600comparativeessay.html' })
+    
+    .state('itl602maindish', {url: '/itl602maindish', templateUrl: './html/artifacts/itl602maindish.html' })
+    
+    .state('itl606classroommanagement', {url: '/itl606classroommanagement', templateUrl: './html/artifacts/itl606classroommanagement.html' })
+    .state('itl606signatureassignment', {url: '/itl606signatureassignment', templateUrl: './html/artifacts/itl606signatureassignment.html' })
+
     .state('itl608literacylearningplan', {url: '/itl608literacylearningplan', templateUrl: './html/artifacts/itl608literacylearningplan.html' })
-    .state('itl608signatureassignment', {url: '/itl608signatureassignment', templateUrl: './html/artifacts/itl608signatureassignment.html' })
+    .state('itl608signatureassignment', {url: '/itl608signatureassignment', templateUrl: './html/artifacts/itl608signatureassignment.html' })    
+
     // .state('developer', {url: '/developer', templateUrl: './html/developer.html' })
     .state('resume', {url: '/resume', templateUrl: './html/resume.html' })
     // .state('blog', {url: '/blog', templateUrl: './html/blog.html' })
